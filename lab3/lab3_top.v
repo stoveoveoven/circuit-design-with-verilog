114293e42f86cbf1b5ae192a337bda98bf49d823
muhan-alan-li <alanli8519@gmail.com>
2021-10-23 16:21:50 -0700

    Update lab3_top.v

--- lab3/lab3_top.v
+++ lab3/lab3_top.v
@@ -1,12 +1,12 @@
-//top level module
-module lab5(key, sw, hex);
-    input [3:0] key, sw;
-    output [5:0] hex;
+module lab3_top(SW,KEY,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,LEDR);
+  input [9:0] SW;
+  input [3:0] KEY;
+  output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+  output [9:0] LEDR; // optional: use these outputs for debugging on your DE1-SoC
 
-    //testing github func
+  
 
-
-
+  
 endmodule
 
 //State machine for numerical inputs
