506587a86a57da76e4a7bd86df0cef8a532b9a45
Steven Shao <sshao3466@gmail.com>
2021-10-24 20:26:56 -0700

    refixed again success

--- lab3/lab3_top_tb.v
+++ lab3/lab3_top_tb.v
@@ -1,29 +1,20 @@
 // TESTBENCHES
+
 module test_success;
-  
 `define A 4'b0001
 `define B 4'b0010
 `define C 4'b0011
 `define D 4'b0100
 `define E 4'b0101
 `define F 4'b0110
-  
+
     reg [9:0] SW;
     reg [3:0] KEY;
     wire [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
     wire [9:0] LEDR; // optional: use these outputs for debugging on your DE1-SoC
 
     wire [4:0] stateToLED;
-  
-    reg[23:0] STATES;
-
-    assign STATES[3:0] = `A;
-    assign STATES[7:4] = `B;
-    assign STATES[11:8] = `C;
-    assign STATES[15:12] = `D;
-    assign STATES[19:16] = `E;
-    assign STATES[23:20] = `F;
-
+    
     lab3_top DUT(SW,KEY,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,LEDR);
 
     initial begin
