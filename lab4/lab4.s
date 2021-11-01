b56f117703f482a165bad1f0130170cb9cb568f0
muhan-alan-li <alanli8519@gmail.com>
2021-10-31 17:31:45 -0700

    added some code
    idek if this is correct bro

--- lab4/lab4.s
+++ lab4/lab4.s
@@ -1,6 +1,22 @@
         .globl binary_search
         
 binary_search:
-        
+
+        //preloop value assignment
+        mov r4, #0              //StartIndex
+        sub r9 r2 #1            //
+        str r9 r5               //EndIndex
+        mov r6, r5 LSR #1       //MiddleIndex
+        mvn r7, #0              //KeyIndex
+        str #1 r8               //NumIters
+
+        ldr 
+Loop:   CMP r7, NEG #0
+        bne Exit
+
+
+        B Loop
+Exit:   
+
 
         
