97218dd907f99fe4590a9ac891429055033ce04a
muhan-alan-li <alanli8519@gmail.com>
2021-10-31 19:50:42 -0700

    not sure how to implement > function in this dogshit langauge

--- lab4/lab4.s
+++ lab4/lab4.s
@@ -4,16 +4,24 @@ binary_search:
 
         //preloop value assignment
         mov r4, #0              //StartIndex
-        sub r9 r2 #1            //
-        str r9 r5               //EndIndex
-        mov r6, r5 LSR #1       //MiddleIndex
+        sub r9, r2, #1            //
+        str r9, r5               //EndIndex
+        mov r6, lsr r5, #1       //MiddleIndex
         mvn r7, #0              //KeyIndex
-        str #1 r8               //NumIters
+        str #1, r8               //NumIters
 
-        ldr 
-Loop:   CMP r7, NEG #0
+        ldr r10, NEG #0
+Loop:   cmp r7, NEG #0          //condition for while loop to continue
+        bne Exit                //go to exit if startIndex != -1
+        
+        cmp r4, r5
         bne Exit
 
+        ldr r9, [r0, r6]
+        cmp r9, r1
+        str r1, r7
+
+        cmpge 
 
         B Loop
 Exit:   
