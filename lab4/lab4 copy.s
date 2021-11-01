22149d7fcc9919b6a9709fedf3f48c21f3d81839
Steven Shao <sshao3466@gmail.com>
2021-11-01 12:11:28 -0700

    added some comments

--- lab4/lab4 copy.s
+++ lab4/lab4 copy.s
@@ -1,12 +1,5 @@
         .globl binary_search
 binary_search:
-        /* Assignments guide:
-                r0 = data (*numbers)
-                r1 = key
-                r2 = length
-                r3 = DONT FUCK WITH THIS
-         */
-
         mov r4, #0 // startindex
         sub r5, r2,#1 // endindex
         mov r6, r5,lsr #1 // middleindex
@@ -15,16 +8,14 @@ binary_search:
 
 loop:
         cmp r4,r5
-
         bgt Exit // first if cond, if startindex big then break
 
         ldr r9,[r0,r6, lsl #2] // get number at middle index
-
         cmp r9,r1
         bgt Front // if the number in middle is bigger than key check front
         blt Back // if the number in the middle is less than key check back
 
-        mov r7, r6
+        mov r7, r6 // else number at middle is the key
 	b Exit
 Front:
         sub r5,r6,#1 // reassign endindex
@@ -34,11 +25,11 @@ Back:
         b helper
 helper:
         rsb r9, r8,#0 // negative numiters
-        str r9,[r0,r6,LSL #2] // store the neg iter at middleindex NOT NECESSARY
+        str r9,[r0,r6,LSL #2] // store the neg iter at middleindex because aesthetic
         sub r10,r5,r4 // helper calc
         add r6,r4,r10,LSR #1 // redefine middle index
-        add r8,r8,#1
+        add r8,r8,#1 // increment
         b loop
 Exit:
-        mov r0,r7
-        mov pc, lr
+        mov r0,r7 // set r0 to key as well
+        mov pc, lr // necessary snippet
