79616e2bee65a05dc57f6516185fc97f5c525e0b
muhan-alan-li <alanli8519@gmail.com>
2021-11-12 17:19:42 -0800

    updated skeleton

--- lab5/lab5_top.v
+++ lab5/lab5_top.v
@@ -1,1 +1,30 @@
-fuck me 
+//top level module for the RISC
+module labt_top();
+
+endmodule
+
+//a register, parameter specifies how many bits is stored
+module reg()
+
+endmodule
+
+//Standard multiplexer
+module MUX();
+
+endmodule
+
+//Shifter units that shifts left or right depending on input
+module shifter();
+
+endmodule
+
+//a register with load enable
+module regLoad();
+
+endmodule
+
+//Arithmetic Logic unit, handles ADD/SUB
+module ALU();
+
+endmodule
+
