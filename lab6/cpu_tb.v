8499aebde5afab235603a684f05c436d5b605ca2
Steven Shao <sshao3466@gmail.com>
2021-11-21 15:23:55 -0800

    ADD seems to be completely functional, now working on CMP

--- lab6/cpu_tb.v
+++ lab6/cpu_tb.v
@@ -53,19 +53,44 @@ module cpu_tb;
         // 1010000101001000
         in = 16'b1010000101001000;
         load = 1'b1;
-    
         #10; // load instruction in
-
         load = 1'b0;
         s = 1'b1; 
+        #60; // now do these instructions
+        s = 1'b0;
 
-        #80; // now do these instructions
+        // ADD R2,R1,R0,LSL#1 should be 16
+        // 1010000101001000
+        in = 16'b1010000101001000;
+        load = 1'b1;
+        #10; // load instruction in
+        load = 1'b0;
+        s = 1'b1; 
+        #60; // now do these instructions
+        s = 1'b0;
 
+        // ADD R4,R3,R2,LSR#1 should be 14+8 = 22
+        // 1010001110010010
+        in = 16'b1010001110010010;
+        load = 1'b1;
+        #10; // load instruction in
+        load = 1'b0;
+        s = 1'b1; 
+        #60; // now do these instructions
+        s = 1'b0;
+
+        // MOV R0, #69
+        // 1101000101000101
+        in = 16'b1101000101000101;
+        load = 1'b1;#10; // load instruction in
+        load = 1'b0;
+        s = 1'b1;
+        #20; // now do these instructions, 2 clock cycles + 1 for load = 3 clock cycles for moving number into reg
         s = 1'b0;
 
 
 
-
+        #10;
         $stop;
     end
 
