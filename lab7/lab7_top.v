89e5da51a688d2ebefb26c654c8b70810363aa71
muhan-alan-li <alanli8519@gmail.com>
2021-11-27 16:51:46 -0800

    ?

--- lab7/lab7_top.v
+++ lab7/lab7_top.v
@@ -14,29 +14,50 @@ module lab7_top(KEY, SW, LEDR, HEX), HEX1, HEX2, HEX3, HEX4, HEX5);
     assign dout_enable = (1'b0 == mem_addr[8:8]) && (`MREAD  == mem_cmd);
 
     assign r_data      = dout_enable ? dout : {16{1'bz}};
-    
 
-    ram MEM (   .clk(~KEY[0]),      .read_address(mem_addr[7:0]),   .write_address(mem_addr[7:0]), 
-                .write(write),      .din(w_data),                   .dout(dout));
 
-    cpu CPU (   .clk(~KEY[0]),      .reset(~KEY[1]),                .r_data(r_data), 
-                .mem_cmd(mem_cmd),  .mem_addr(mem_addr)             .w_data(w_data));
+    ram     MEM (   .clk(~KEY[0]),          .read_address(mem_addr[7:0]),   .write_address(mem_addr[7:0]), 
+                    .write(write),          .din(w_data),                   .dout(dout));
+
+    cpu     CPU (   .clk(~KEY[0]),          .reset(~KEY[1]),                .r_data(r_data), 
+                    .mem_cmd(mem_cmd),      .mem_addr(mem_addr)             .w_data(w_data));
+
+    SWctrl  SWctrl( .SW(SW),                .memCmdIn(mem_cmd),             .memAddrIn(mem_addr),
+                    .readDataOut(r_data));
+
+    LEDctrl LEDctrl(.LEDR(LEDR),            .memCmdIn(mem_cmd),             .memAddrIn(mem_addr),
+                    .writeDataIn(w_data),   .clk(~KEY[0]));
 endmodule
 
-module input_iface(clk, SW, ir, LEDR);
-  input clk;
-  input [9:0] SW;
+module SWctrl(SW, memCmdIn, memAddrIn, readDataOut);
+    input [8:0] SW, memAddrIn;
+    input [1:0] memCmdIn;
 
-  output [15:0] ir;
-  output [7:0] LEDR;
+    output [15:0] readDataOut;
 
-  wire sel_sw = SW[9];  
-  wire [15:0] ir_next = sel_sw ? {SW[7:0],ir[7:0]} : {ir[15:8],SW[7:0]};
+    wire triStateCtrl;
 
-  vDFF #(16) REG(clk,ir_next,ir);
+    assign triStateCtrl = (memCmdIn == `MREAD) && (memAddrIn == 9'h140) //not sure about this second part
+    assign readDataOut[15:8] = triStateCtrl ? 8'h00 : {8{1'bx}};
+    assign readDataOut[7:0]  = triStateCtrl ? SW    : {8{1'bx}};        //not sure to use x or z
+endmodule
 
-  assign LEDR = sel_sw ? ir[7:0] : ir[15:8];  
-endmodule         
+module LEDctrl(LEDR, memCmdIn, memAddrIn, writeDataIn, clk);
+    input [8:0] memAddrIn;
+    input [7:0] writeDataIn;
+    input [1:0] memCmdIn;
+    input clk;
+
+    output [7:0] LEDR;
+
+    wire load;
+
+    assign load = (memCmdIn == `MWRITE) && (memAddrIn == 9'h100); 
+
+    regLoad #(8) LEDreg(writeDataIn, load, clk, LEDR);
+endmodule
+
+
 
 //Standard vDFF
 module vDFF(clk,D,Q);
@@ -92,67 +113,4 @@ module Dec38(in, out);
             default: out = 8'bxxxxxxxx;
         endcase
     end
-endmodule
-
-// The sseg module below can be used to display the value of datpath_out on
-// the hex LEDS the input is a 4-bit value representing numbers between 0 and
-// 15 the output is a 7-bit value that will print a hexadecimal digit.  You
-// may want to look at the code in Figure 7.20 and 7.21 in Dally but note this
-// code will not work with the DE1-SoC because the order of segments used in
-// the book is not the same as on the DE1-SoC (see comments below).
-
-module sseg(in, segs);
-  //
-  // One bit per segment. On the DE1-SoC a HEX segment is illuminated when
-  // the input bit is 0. Bits 6543210 correspond to:
-  //
-  //    0000
-  //   5    1
-  //   5    1
-  //    6666
-  //   4    2
-  //   4    2
-  //    3333
-  //
-  // Decimal value | Hexadecimal symbol to render on (one) HEX display
-  //             0 | 0
-  //             1 | 1
-  //             2 | 2
-  //             3 | 3
-  //             4 | 4
-  //             5 | 5
-  //             6 | 6
-  //             7 | 7
-  //             8 | 8
-  //             9 | 9
-  //            10 | A
-  //            11 | b
-  //            12 | C
-  //            13 | d
-  //            14 | E
-  //            15 | F
-    input [3:0] in;
-    output reg [6:0] segs;
-
-    always@(in)begin
-        case(in)
-            4'b0000 : segs = 7'b1000000;
-            4'b0001 : segs = 7'b1111001;
-            4'b0010 : segs = 7'b0100100;
-            4'b0011 : segs = 7'b0110000;
-            4'b0100 : segs = 7'b0011001;
-            4'b0101 : segs = 7'b0010010;
-            4'b0110 : segs = 7'b0000010;
-            4'b0111 : segs = 7'b1111000;
-            4'b1000 : segs = 7'b0000000;
-            4'b1001 : segs = 7'b0011000;
-            4'b1010 : segs = 7'b0001000;
-            4'b1011 : segs = 7'b0000011;
-            4'b1100 : segs = 7'b1000110;
-            4'b1101 : segs = 7'b0100001;
-            4'b1110 : segs = 7'b0000110;
-            4'b1111 : segs = 7'b0001110;
-        endcase
-    end
-    
 endmodule
